212e8c03cb30c4b3bf230640dda11c37ca35e2c5
Kamlesh-Negi <125206730+Kamlesh-Negi@users.noreply.github.com>
2024-01-15 14:51:56 +0530

    commit-1

---
+++ Functions_Methods/Add.java
@@ -0,0 +1,20 @@
+package JavaLearning.Functions;
+
+import java.util.Scanner;
+
+public class Add {
+    static int Sum()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("enter second number: ");
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum = "+Sum());
+    }
+}
